416c5326f9aaa6496fd6ca98ec59a505aeb55f24
Mike Yan Michelis <mike.michelis@tum.de>
2019-08-26 10:29:07 +0200

    Random Scenes: Completed random initial density and velocities for FLIP and SDF

--- apps/random_scene_generator.py
+++ apps/random_scene_generator.py
@@ -4,13 +4,13 @@ from phi.tf.flow import *
 from phi.math.sampled import *
 
 
-def insert_circles(field, centers, radii):
+def insert_circles(field, centers, radii, values=None):
     """
-Field should be a density/active mask field with shape [batch, coordinate_dimensions, 1]
+Field should be a density/active mask/velocity field with shape [batch, coordinate_dimensions, components].
 Centers should be given in index format (highest dimension first) and values should be integers that index into the field. Can be a list of coordinates.
 Radii can be a single value if it is the same for all centers, otherwise specify a radius for every center value in the list of centers.
+Values should specify the vector that goes into the entry of the corresponding circle (list of vectors if there are multiple centers).
     """
-    assert field.shape[-1] == 1
 
     indices = indices_tensor(field).astype(int)
     indices = math.reshape(indices, [indices.shape[0], -1, indices.shape[-1]])[0]
@@ -20,9 +20,18 @@ Radii can be a single value if it is the same for all centers, otherwise specify
 
     # Loop through entire field and mark the cells that are in the circle
     for index in indices:
-        if (math.sum((index - centers)**2, axis=-1) <= radii**2).any():
-            field_index = [slice(None)] + math.unstack(index) + [0]
-            field[field_index] = 1
+        where_circle = (math.sum((index - centers)**2, axis=-1) <= radii**2)
+
+        if (where_circle).any():
+            field_index = [slice(None)] + math.unstack(index) + [slice(None)]
+
+            if values is None:
+                # Insert scalar density/fluid mask
+                field[field_index] = 1
+            else:
+                # Insert vector field
+                values_index = math.where(where_circle)[0]     # Always take first possible circle
+                field[field_index] = values[values_index]
 
     return field
 
@@ -39,22 +48,25 @@ class RandomLiquid(TFModel):
         self.gravity = -4.0
 
         self.initial_density = zeros(domain.grid.shape())
-
+        # Initial velocity different for FLIP, so set it separately over there
+        self.initial_velocity = zeros(domain.grid.staggered_shape())
 
         number_of_circles = np.random.randint(1, min(self.size)/2)
-        centers = np.array([np.random.randint(i, size=number_of_circles) for i in self.size]).reshape([-1, 2])
+        centers = np.array([np.random.randint(i, size=number_of_circles) for i in self.size]).reshape([-1, len(self.size)])
         radii = np.random.uniform(0, min(self.size)/number_of_circles, size=number_of_circles)
+        velocities = np.array([np.random.uniform(-min(self.size)/4, min(self.size)/4, size=number_of_circles) for _ in self.size]).reshape([-1, len(self.size)])
 
         self.initial_density = insert_circles(self.initial_density, centers, radii)
+        self.initial_velocity = StaggeredGrid(insert_circles(self.initial_velocity.staggered, centers, radii, velocities))
 
 
         self.sess = Session(Scene.create('liquid'))
         # Choose whether you want a particle-based FLIP simulation or a grid-based SDF simulation
-        self.flip = False
+        self.flip = True
         if self.flip:
             # FLIP simulation
             self.particles_per_cell = 4
-            self.initial_velocity = 0.0
+            self.initial_velocity = np.random.uniform(-min(self.size)/4, min(self.size)/4, size=len(self.size))
             
             self.liquid = world.FlipLiquid(state_domain=domain, density=self.initial_density, velocity=self.initial_velocity, gravity=self.gravity, particles_per_cell=self.particles_per_cell)
 
@@ -70,7 +82,6 @@ class RandomLiquid(TFModel):
         else:
             # SDF simulation
             self.distance = max(self.size)
-            self.initial_velocity = zeros(domain.grid.staggered_shape())
 
             self.liquid = world.SDFLiquid(state_domain=domain, density=self.initial_density, velocity=self.initial_velocity, gravity=self.gravity, distance=self.distance)
 
@@ -92,14 +103,21 @@ class RandomLiquid(TFModel):
 
     def action_reset(self):
         self.initial_density = zeros(self.liquid.grid.shape())
+        self.initial_velocity = zeros(self.liquid.grid.staggered_shape())
+
         number_of_circles = np.random.randint(1, min(self.size)/2)
-        centers = np.array([np.random.randint(i, size=number_of_circles) for i in self.size]).reshape([-1, 2])
-        radii = np.random.randint(min(self.size)/number_of_circles, size=number_of_circles)
+        centers = np.array([np.random.randint(i, size=number_of_circles) for i in self.size]).reshape([-1, len(self.size)])
+        radii = np.random.uniform(0, min(self.size)/number_of_circles, size=number_of_circles)
+        velocities = np.array([np.random.uniform(-min(self.size)/4, min(self.size)/4, size=number_of_circles) for _ in self.size]).reshape([-1, len(self.size)])
 
         self.initial_density = insert_circles(self.initial_density, centers, radii)
+        self.initial_velocity = StaggeredGrid(insert_circles(self.initial_velocity.staggered, centers, radii, velocities))
+        
 
         if self.flip:
             self.liquid.points = random_grid_to_coords(self.initial_density, self.particles_per_cell)
+
+            self.initial_velocity = np.random.uniform(-min(self.size)/4, min(self.size)/4, size=len(self.size))
             self.liquid.velocity = zeros_like(self.liquid.points) + self.initial_velocity
 
         else:
