be5266928b906ff0d4399dc72b592965a474f30d
RCCG <r.greif@outlook.com>
2020-01-10 17:13:42 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../phi/'))
 from sphinx.ext import apidoc
-apidoc.main(['-f', '-f', '-M', '-T', '../tcms_api'])
+apidoc.main(['-f', '-P', '../phi/'])
 
 
 # -- Project information -----------------------------------------------------
