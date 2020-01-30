56448029c627b8d017a96cf54865f05bf19aa9d5
the-rccg <r.greif@outlook.com>
2020-01-30 12:31:11 +0100

    Tensorflow 2.0 import compatibility mode

--- phi/tf/flow.py
+++ phi/tf/flow.py
@@ -7,3 +7,7 @@ from .world import *
 from .data import *
 from .util import *
 import tensorflow as tf
+if tf.__version__[0] == '2':
+    tf = tf.compat.v1
+    tf.disable_eager_execution()
+
--- tests/test_tensorflow.py
+++ tests/test_tensorflow.py
@@ -1,7 +1,11 @@
 from unittest import TestCase
 
 import numpy
-import tensorflow
+import tensorflow as tf
+if tf.__version__[0] == '2':
+    tf = tf.compat.v1
+    tf.disable_eager_execution()
+
 
 from phi.tf.util import placeholder
 
@@ -14,21 +18,21 @@ from phi.tf.util import variable
 class TestPlaceholder(TestCase):
 
     def test_direct_placeholders(self):
-        tensorflow.reset_default_graph()
+        tf.reset_default_graph()
         p = placeholder([4])
-        self.assertIsInstance(p, tensorflow.Tensor)
+        self.assertIsInstance(p, tf.Tensor)
         numpy.testing.assert_equal(p.shape.as_list(), [4])
         self.assertEqual(p.name, 'Placeholder:0')
         v = variable(math.zeros([2, 2]))
         numpy.testing.assert_equal(v.shape.as_list(), [2, 2])
-        self.assertIsInstance(v, tensorflow.Variable)
+        self.assertIsInstance(v, tf.Variable)
         self.assertEqual(v.name, 'Variable:0')
 
     def test_struct_placeholders(self):
         bounds = box[0:1]  # outside unsafe
         with struct.unsafe():
             obj = ([4], CenteredGrid([1, 4, 1], bounds), ([9], [8, 2]))
-        tensorflow.reset_default_graph()
+        tf.reset_default_graph()
         p = placeholder(obj)
         self.assertEqual(p[0].name, '0:0')
         self.assertEqual(p[1].data.name, '1/data:0')
