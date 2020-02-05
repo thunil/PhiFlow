0033e89f43c2bdfa34b121758d730f0db5a78dfa
the-rccg <r.greif@outlook.com>
2020-02-05 10:25:07 +0100

    Test Periodic Laplace for Plasma

--- tests/test_plasma.py
+++ tests/test_plasma.py
@@ -12,10 +12,14 @@ from phi.physics.plasma import *
 #    tf = tf.compat.v1
 #    tf.disable_eager_execution()
 
+INPUT_CHANGED_MSG = "Side Effect detected: solver causes input array to change"
+OUTPUT_NOT_UNIFORM = "Uniform input does not yield uniform output. Recommendation: visualize array: import matplotlib.pyplot as plt; plt.imshow(array_out); plt.colorbar()"
+
 
 class TestMath(TestCase):
 
     def test_arakawa(self):
+        """ Test the Arakawa Implementation using two functions """
         # Test Sizes
         size = 64
         shape = (size, size)
@@ -42,3 +46,30 @@ class TestMath(TestCase):
         dev = np.abs(np.divide((h_arr - res)[1:-1, 1:-1][mask], h_arr[1:-1, 1:-1][mask]))
         # Check it is in an acceptable boundary
         self.assertLess(np.max(dev), 0.7)
+    
+    def test_laplace(self):
+        """ test laplace for cylindrical behavior """
+        # Setup uniform 3D Array
+        N = 16
+        array_in = np.ones((N, N, N))
+        # Compute laplace for each dimension for circular
+        laplace_axis_z = laplace(array_in, padding='circular', axes=[0])
+        laplace_axis_y = laplace(array_in, padding='circular', axes=[1])
+        laplace_axis_x = laplace(array_in, padding='circular', axes=[2])
+        self.assertEqual(np.max(np.abs(laplace_axis_z-laplace_axis_z.mean())), 0, OUTPUT_NOT_UNIFORM)
+        self.assertEqual(np.max(np.abs(laplace_axis_y-laplace_axis_y.mean())), 0, OUTPUT_NOT_UNIFORM)
+        self.assertEqual(np.max(np.abs(laplace_axis_x-laplace_axis_x.mean())), 0, OUTPUT_NOT_UNIFORM)
+        # Replicate
+        laplace_axis_z = laplace(array_in, padding='replicate', axes=[0])
+        laplace_axis_y = laplace(array_in, padding='replicate', axes=[1])
+        laplace_axis_x = laplace(array_in, padding='replicate', axes=[2])
+        self.assertEqual(np.max(np.abs(laplace_axis_z-laplace_axis_z.mean())), 0, OUTPUT_NOT_UNIFORM)
+        self.assertEqual(np.max(np.abs(laplace_axis_y-laplace_axis_y.mean())), 0, OUTPUT_NOT_UNIFORM)
+        self.assertEqual(np.max(np.abs(laplace_axis_x-laplace_axis_x.mean())), 0, OUTPUT_NOT_UNIFORM)
+        # Wrap
+        laplace_axis_z = laplace(array_in, padding='wrap', axes=[0])
+        laplace_axis_y = laplace(array_in, padding='wrap', axes=[1])
+        laplace_axis_x = laplace(array_in, padding='wrap', axes=[2])
+        self.assertEqual(np.max(np.abs(laplace_axis_z-laplace_axis_z.mean())), 0, OUTPUT_NOT_UNIFORM)
+        self.assertEqual(np.max(np.abs(laplace_axis_y-laplace_axis_y.mean())), 0, OUTPUT_NOT_UNIFORM)
+        self.assertEqual(np.max(np.abs(laplace_axis_x-laplace_axis_x.mean())), 0, OUTPUT_NOT_UNIFORM)
